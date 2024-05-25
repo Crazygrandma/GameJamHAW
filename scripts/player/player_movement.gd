e29fb97b905a1ae821eda31f9d0c80e4279a93ae
FrostyEngine <frostyengine@gmail.com>
2024-05-25 20:07:43 +0200

    Update player_movement.gd
    Animation player fix

--- scripts/player/player_movement.gd
+++ scripts/player/player_movement.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var playerSpeed = 300.0
+@export var playerSpeed = 200.0
 @onready var sprite_2d = $Sprite2D
 @onready var MamaCat = $AnimatedSprite2D
 
@@ -21,26 +21,34 @@ func _input(event):
 func _process(_delta):
 	print(target, position, velocity)
 	
-	if target.x >= position.x: #walking?
+	#if facing == true and target.x == position.x: #idle
+		#MamaCat.play("Idle_L")
+	#else:
+		#MamaCat.play("Idle_R")
+	
+	if target.x > position.x: #walking and player behavior
 		# Player moves to the right
 		MamaCat.play("Walk_R")
-	else:
+	elif target.x < position.x:
 		MamaCat.play("Walk_L")
-	
-	if facing == true && velocity.x == 0: #idle
-		MamaCat.play("Idle_L")
 	else:
-		MamaCat.play("Idle_R")
+		if facing == true: #idle animation
+			MamaCat.play("Idle_L")
+		else:
+			MamaCat.play("Idle_R")
+	
+	
 	
 	target.y = position.y
 	velocity = position.direction_to(target) * playerSpeed
 	
 	if velocity.x < 0:
 		facing = true
-	else:
+	elif velocity.x > 0:
 		facing = false
 		
 	if position.distance_to(target) > 10:
 		move_and_slide()
 	else:
 		position.x = target.x
+		velocity.x = 0
