77f3ac237138f01795df989f55d615d8aa033cb1
FrostyEngine <frostyengine@gmail.com>
2024-05-25 19:29:12 +0200

    Update player_movement.gd

--- scripts/player/player_movement.gd
+++ scripts/player/player_movement.gd
@@ -2,11 +2,13 @@ extends CharacterBody2D
 
 @export var playerSpeed = 300.0
 @onready var sprite_2d = $Sprite2D
+@onready var MamaCat = $MamaCatAnimation
 
-const MAMACAT = preload("res://assets/sprites/player/mamacat2.png")
-const FLIPPEDMAMACAT = preload("res://assets/sprites/player/flippedmamacat.png")
+#const MAMACAT = preload("res://assets/sprites/player/mamacat2.png")
+#const FLIPPEDMAMACAT = preload("res://assets/sprites/player/flippedmamacat.png")
 
 var target = Vector2()
+var facing: bool = false
 
 func _ready():
 	target = position
@@ -17,16 +19,27 @@ func _input(event):
 		target = get_global_mouse_position()
 
 func _process(_delta):
-	print(target, position)
+	print(target, position, velocity)
 	
-	if target.x >= position.x:
+	if target.x >= position.x: #walking?
 		# Player moves to the right
-		sprite_2d.texture = MAMACAT
+		MamaCat.play("Walk_R")
 	else:
-		sprite_2d.texture = FLIPPEDMAMACAT
-		
+		MamaCat.play("Walk_L")
+	
+	if facing == true && velocity.x == 0: #idle
+		MamaCat.play("Idle_L")
+	else:
+		MamaCat.play("Idle_R")
+	
 	target.y = position.y
 	velocity = position.direction_to(target) * playerSpeed
+	
+	if velocity.x < 0:
+		facing = true
+	else:
+		facing = false
+		
 	if position.distance_to(target) > 10:
 		move_and_slide()
 	else:
