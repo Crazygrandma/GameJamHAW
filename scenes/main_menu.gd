474ce0206d1c4e74ca361e81af61374a25f738d9
Albnacht <Albnacht.info@gmx.de>
2024-05-26 13:29:11 +0200

    added Volume Slider

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -13,12 +13,13 @@ func _ready():
 	btn_start.button_down.connect(on_start_pressed)
 	btn_quit.button_down.connect(on_quit_pressed)
 
-
+# Called when quit button is pressed
 func on_quit_pressed() -> void:
 	btn_quit_sound.play()
 	await get_tree().create_timer(0.8).timeout
 	get_tree().quit()
 
+# Called when start button is pressed
 func on_start_pressed() -> void:
 	btn_click_sound.play()
 	await get_tree().create_timer(0.5).timeout
